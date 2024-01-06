93558ed6bc344b5e28e97bc2c1a6fc52a5f4eee0
Siraj 'Sid' Rakhada <sid-git@mindless.co.uk>
2024-01-06 11:46:51 +1100

    Basic usable version - shows chores due today or earlier.

--- chores_nicegui.py
+++ chores_nicegui.py
@@ -5,31 +5,120 @@ from dotenv import dotenv_values
 from nicegui import ui
 
 import requests
+import requests
+from typing import Callable, List
+from nicegui import ui
+from datetime import datetime, date
 
 config = dotenv_values(".env")
-headers = {'GROCY-API-KEY': f'{config["GROCY_API_KEY"]}'}
+headers = {
+    "GROCY-API-KEY": f'{config["GROCY_API_KEY"]}',
+    "Content-Type": "application/json",
+}
 grocy_host = config["GROCY_HOST"]
+complete_chore_url = "/api/chores/"
+
 
 def update_users():
-    response = requests.get(f'{grocy_host}/api/users', headers=headers)
+    response = requests.get(f"{grocy_host}/api/users", headers=headers)
     users_json = response.json()
-    users = {user['id']: user['display_name'] for user in users_json}
+    users = {user["id"]: user["display_name"] for user in users_json}
     return users, users_json
 
+
 def update_chores():
-    response = requests.get(f'{grocy_host}/api/chores?order=next_estimated_execution_time', headers=headers)
+    response = requests.get(
+        f"{grocy_host}/api/chores?order=next_estimated_execution_time", headers=headers
+    )
     chores_json = response.json()
-    chores = {chore['id']: chore['chore_name'] for chore in chores_json}
+    today = date.today()
+
+    chores = {
+        chore["id"]: chore["chore_name"]
+        for chore in chores_json
+        if datetime.strptime(
+            chore["next_estimated_execution_time"], "%Y-%m-%d %H:%M:%S"
+        ).date()
+        <= today
+    }
+    print(chores_json)
     return chores, chores_json
 
-chores, chores_json = update_chores()
-users, users_json = update_users()
 
 @dataclass
 class ChoreItem:
     id: int
     name: str
-    # done: bool = False
+
+    def complete(self) -> None:
+        execute_chore(self)
+        ui.notify(f"Chore {self.name} completed by {users[state.selected_user]}")
+
+
+def execute_chore(item: ChoreItem):
+    # Get the current date and time
+    current_time = datetime.now()
+
+    # Format the datetime object as a string
+    time_string = current_time.strftime("%Y-%m-%dT%H:%M:%S")
+
+    print(time_string)  # Outputs: '2024-01-05 14:48:26' (for example)
+    print(
+        f"Trying to execute chore id: {item.id} with {grocy_host}{complete_chore_url}{item.id}/execute"
+    )
+    try:
+        response = requests.post(
+            f"{grocy_host}{complete_chore_url}{item.id}/execute",
+            headers=headers,
+            json={
+                "tracked_time": time_string,
+                "done_by": state.selected_user,
+                "skipped": "false",
+            },
+        )
+        print(f"chore id: {item.id} executed")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    if response.status_code == 200:
+        ui.notify("Successfully executed chore")
+    elif response.status_code == 400:
+        ui.notify(f"Error creating: {response.json().get('error_message')}")
+        print(f"Error creating: {response.json().get('error_message')}")
+    elif response.status_code == 500:
+        ui.notify(f"Error creating: {response.json().get('error_message')}")
+        print(f"Error creating: {response.json().get('error_message')}")
+    else:
+        ui.notify(f"Unclear what occurred, response: {response.status_code}")
+        print(f"Unclear what occurred, response: {response.status_code}")
+
+
+chores, chores_json = update_chores()
+users, users_json = update_users()
+
+
+userlist = []
+for user in users:
+    userlist.append(user)
+
+
+@dataclass
+class State:
+    selected_user: int = 0
+
+    def update_selected_user(self, id):
+        self.selected_user = id
+
+
+state = State()
+
+
+# @dataclass
+# class ChoreItem:
+#     id: int
+#     name: str
+
+#     def complete(self) -> None:
+#         ui.notify(f"Chore {self.name} completed by {users[state.selected_user]}")
 
 
 @dataclass
@@ -42,65 +131,74 @@ class ChoreList:
         self.items.append(ChoreItem(id, name))
         self.on_change()
 
-    def complete(self, item: ChoreItem) -> None:
-        pass
+    def complete(self, item: ChoreItem):
+        item.complete()
         self.on_change()
 
-def show():
-    result = dialog
-    if result == 'Yes':
-        ui.notify('Chore completed')
-    else:
-        ui.notify('Chore not completed')
 
-def replace(item):
-    currentuser=users[usertoggle.value]
-    # currentchore=choreslist.item[name]
+def replace(chore_item, state):
+    if state.selected_user < 1:
+        ui.notify(f"Pick a user!  {state.selected_user}")
+        return
     dialog.clear()
     with dialog, ui.card():
-        ui.label(f'Completing chore as {currentuser}')
+        ui.label(f"Completing chore {chore_item.name} as {users[state.selected_user]}")
         with ui.row():
-            ui.button('Yes', on_click=lambda: dialog.submit('Yes'))
-            ui.button('No', on_click=lambda: dialog.submit('No'))
+            ui.button(
+                "Yes",
+                on_click=lambda: (
+                    dialog.submit("Yes"),
+                    choreslist.complete(chore_item),
+                    dialog.close(),
+                ),
+            )
+            ui.button("No", on_click=dialog.close)
     dialog.open()
 
 
 @ui.refreshable
 def chore_ui():
     if not choreslist.items:
-        ui.label('List is empty.').classes('mx-auto')
+        ui.label("List is empty.").classes("mx-auto")
         return
-    for item in choreslist.items:
-        with ui.row().classes('items-center'):
-            # ui.checkbox(value=item.done, on_change=chore_ui.refresh).bind_value(item, 'done')
-            ui.button(item.name, on_click=lambda: replace(item)) #.classes('flex-grow').bind_value(item, 'name')
-            # ui.button(on_click=lambda item=item: choreslist.remove(item), icon='delete').props('flat fab-mini color=grey')
+    for chore_item in choreslist.items:
+        with ui.row().classes("items-center"):
+            ui.button(
+                chore_item.name,
+                on_click=lambda chore_item=chore_item: replace(chore_item, state),
+            )
+
 
 with ui.dialog() as dialog, ui.card():
-    ui.label(f'Completing chore as') #{users[usertoggle.value]}')
+    ui.label(f"Completing chore as")  # {users[usertoggle.value]}')
     with ui.row():
-        ui.button('Yes', on_click=lambda: dialog.submit('Yes'))
-        ui.button('No', on_click=lambda: dialog.submit('No'))
-
-usertoggle=ui.toggle(users, on_change=chore_ui.refresh()).props('inline')
-# ui.button("Refresh", on_click=lambda: ui.refresh())
-# ui.toggle(chores)
-
-# with ui.dialog() as dialog, ui.card():
-#     ui.label(f'Completing chore as {users[usertoggle.value]}')
-#     with ui.row():
-#         ui.button('Yes', on_click=lambda: dialog.submit('Yes'))
-#         ui.button('No', on_click=lambda: dialog.submit('No'))
+        ui.button("Yes", on_click=lambda: dialog.submit("Yes"))
+        ui.button("No", on_click=dialog.close)
 
 
+def get_key(my_dict, val):
+    for key, value in my_dict.items():
+        if val == value:
+            return key
+    return 0
 
-choreslist = ChoreList('Chores', on_change=chore_ui.refresh)
+
+usertoggle = ui.toggle(
+    users, on_change=lambda: (set_current_user(), chore_ui.refresh())
+).props("inline")
+
+
+def set_current_user():
+    print(usertoggle.value)
+    state.update_selected_user(usertoggle.value)
+
+
+choreslist = ChoreList("Chores", on_change=chore_ui.refresh)
 with ui.row():
     for chore in chores:
         choreslist.add(chore, chores[chore])
 
-with ui.card().classes('w-80 items-stretch'):
-    # ui.label().bind_text_from(choreslist, 'title').classes('text-semibold text-2xl')
+with ui.card().classes("w-80 items-stretch"):
     chore_ui()
 
 ui.run()
